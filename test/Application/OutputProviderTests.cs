b8fb0ceeb1e4046c02525883a8c53e566f344592
LordViktor099 <vacs099@gmail.com>
2025-02-03 23:24:01 -0600

    Moving to src and test folders

---
+++ test/Application/OutputProviderTests.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using NUnit.Framework.Internal;
+using SPAN_League_ranking.Application;
+using SPAN_League_ranking.Domain;
+using SPAN_League_ranking.Infrastructure;
+
+namespace SPAN_League_ranking_tests.Application
+{
+    public class OutputProviderTests
+    {
+        [Test]
+        public void WhenSentToOutputCalled_ShouldCall_WriteToFile()
+        {
+            Mock<ITxtFileOuputFormatter> txtFileOutoutFormatterMock = new Mock<ITxtFileOuputFormatter>();
+
+            var sut = new OutputProvider(txtFileOutoutFormatterMock.Object);
+
+            sut.SendToOutput(new Dictionary<ITeam, int>());
+
+            txtFileOutoutFormatterMock.Verify(x => x.WriteToFile(It.IsAny<string>(), It.IsAny<Dictionary<ITeam, int>>()), Times.Once());
+        }
+    }
+}
---
+++ test/Application/RankProviderTests.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using Moq;
+using SPAN_League_ranking.Application;
+using SPAN_League_ranking.Domain;
+
+namespace SPAN_League_ranking_tests.Application
+{
+    public class RankProviderTests
+    {
+        [Test]
+        public void GenerateRankForMatchday_ShouldCall_PointAwarderAssignPoints()
+        {
+            // Arrange
+            Mock<IPointAwarder> pointAwarderMock = new Mock<IPointAwarder>();
+            pointAwarderMock.Setup(x => x.AssignPoints(It.IsAny<IResult>())).
+                Returns(CreatePointAssignation());
+
+            List<IResult> results = CreateResults();
+            HashSet<ITeam> teams = CreateTeams();
+            Team teamA = CreateTeam("TeamA");
+
+            var sut = new RankProvider(pointAwarderMock.Object);
+
+            // Act
+            var actualValue = sut.GenerateRankForMatchday(results, teams);
+
+            // Assert
+            pointAwarderMock.Verify(x => x.AssignPoints(It.IsAny<IResult>()), Times.Once());
+            actualValue.Count.Should().Be(2);
+            actualValue[teamA].Should().Be(1);
+        }
+
+        private PointAssignation CreatePointAssignation()
+        {
+            return new PointAssignation()
+            {
+                AwayTeam = CreateTeam("TeamA"),
+                HomeTeam = CreateTeam("TeamB"),
+                AwayTeamPointsAwarded = 1,
+                HomeTeamPointsAwarded = 1
+            };
+        }
+
+        private List<IResult> CreateResults()
+        {
+            return new List<IResult>() { 
+                new Result {
+                    AwayTeam = CreateTeam("TeamA"),
+                    HomeTeam = CreateTeam("TeamB"),
+                    Score = new Score() 
+                    { AwayScore = 1, HomeScore = 1 } 
+                } 
+            };
+        }
+
+        private HashSet<ITeam> CreateTeams()
+        {
+            return new HashSet<ITeam>() { 
+                CreateTeam("TeamA"), 
+                CreateTeam("TeamB")  
+            };
+
+        }
+
+        private Team CreateTeam(string name)
+        {
+            return new Team()
+            {
+                Name = name
+            };
+        }
+
+
+    }
+
+}
