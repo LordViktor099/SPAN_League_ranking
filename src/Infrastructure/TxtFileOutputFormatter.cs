dc3cae50788ff62518e97530383b292ab67df3b0
LordViktor099 <vacs099@gmail.com>
2025-02-04 00:09:55 -0600

    Fixing list formatting

--- src/Infrastructure/TxtFileOutputFormatter.cs
+++ src/Infrastructure/TxtFileOutputFormatter.cs
@@ -12,10 +12,19 @@ namespace SPAN_League_ranking.Infrastructure
         public void WriteToFile(string filePath, Dictionary<ITeam, int> orderedRank)
         {
             List<string> output = new List<string>();
+            int listNumber = 1;
+            int lastPoints = 1;
+            int idx = 0;
             foreach(var team in orderedRank.Keys)
             {
+                idx++;
+                if ( lastPoints != orderedRank[team])
+                {
+                    lastPoints = orderedRank[team];
+                    listNumber = idx;
+                }
                 var ptTerminator = orderedRank[team] == 1 ? "pt" : "pts";
-                output.Add($"{team.Name}, {orderedRank[team]} {ptTerminator}");
+                output.Add($"{listNumber}.{team.Name}, {orderedRank[team]} {ptTerminator}");
             }
             File.WriteAllLines(filePath, output);
         }
